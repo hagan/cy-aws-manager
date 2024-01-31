e15ffbeaf156847f3f9b921f659abd91d607bb43
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-01-30 20:31:33 -0700

    fixed bug in extract-vars.sh script

--- extract-vars.sh
+++ extract-vars.sh
@@ -6,13 +6,17 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+if [ "x${GITHUB_OUTPUT}" == "x" ]; then
+  GITHUB_OUTPUT=/dev/null
+fi
+
 ENV_VARS=($(awk '!/^#/ && NF' $1 | awk -F '[ =]' '{if (NF > 0) print $1}' | tr '\n' ' '))
 
 for elm in "${ENV_VARS[@]}"; do
-  elm_val=$(make -f Makefile --include config.mk -p -n | grep "^$elm.*=")
+  elm_val=$(make -f Makefile --include config.mk -p -n | grep "^$elm.*=" | awk -F'=' '{ gsub(/^[ \t]+/, "", $2); print $2 }')
   declare "$elm=$elm_val"
   export "$elm"
   ## deprecated set-out -> now just pipe it
-#   echo "::set-output name=$elm::$elm_val"
+  # echo "::set-output name=$elm::$elm_val"
   echo "${elm}=${elm_val}" >> $GITHUB_OUTPUT
 done
